8e1dcade8b3bea263546ad7563aa0a09de2bbf4e
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-25 23:23:52 +0200

    Update init_db.sql

--- db/init_db.sql
+++ db/init_db.sql
@@ -6,7 +6,8 @@ CREATE TYPE user_role AS ENUM ('owner', 'admin');
 CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   email TEXT NOT NULL UNIQUE,
-  name TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   created_by UUID REFERENCES users(id) ON DELETE SET NULL,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
